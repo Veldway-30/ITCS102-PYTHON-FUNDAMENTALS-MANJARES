451ca63b1e9ad72f901091423f45a8ec96fc2643
Cj <cjmanjares447@gmail.com>
2025-08-15 15:04:29 +0800

    Rename Activity3.py to Code_Challenge1.py

--- Activity3.py
+++
@@ -1,3 +0,0 @@
-name = input("What is your name .?")
-
-print("\t\t\t\t* \n\n \t\t\t*\t\t* \n\n \t\t*\t\t\t\t*\n\n \t*\t\t\t Hello \t\t\t* \n\n *\t\t\t\t", name,"\t\t\t\t* \n\n \t*\t\t\t\t\t\t* \n\n \t\t*\t\t\t\t* \n\n \t\t\t*\t\t* \n\n \t\t\t\t*")
