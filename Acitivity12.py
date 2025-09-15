b403208d96a0245db2c73e521c147a9e67eff308
Cj <cjmanjares447@gmail.com>
2025-09-15 14:42:51 +0800

    Delete Acitivity12.py

--- Acitivity12.py
+++
@@ -1,8 +0,0 @@
-
-print("Hello")
-number = "0"
-for ikot in range (1, 10, 1):
-    num = eval(input(ikot, "Please input a number: "))
-    number += num
-
-print("The total value of the number is ", number)
