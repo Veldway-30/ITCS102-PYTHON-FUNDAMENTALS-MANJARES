5d707cd5326ae85317ed23f5f7def3468b11760a
Cj <cjmanjares447@gmail.com>
2025-09-19 11:29:29 +0800

    This is my file in github

---
+++ activity15.py
@@ -0,0 +1,8 @@
+print("MANGA-MANWA-CARTOONS")
+
+likes = input("Please choose what you want?: ")
+
+print("2000s-2010")
+time = input ("Please choose what era: ")
+print(f"you have choosen {likes.upper} - era {time}")
+
